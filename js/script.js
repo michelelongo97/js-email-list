7ffdfb36483a36824effdef41498455040159d4c
michelelongo97 <m.longo7x@gmail.com>
2025-01-08 18:12:55 +0100

    feat: add for cycle and bootstrap

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const emailElm = document.getElementById("email")
 
+for(let i = 0; i < 10; i++) {
 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 .then(function(response){
 let email = response.data.response;
-emailElm.innerHTML += email;
+emailElm.innerHTML += `<li>${email}</li>`;
 })
-
+}
